f97d90f275793b2f33f1423cac032745b7fa2efd
tatsoni_desktop_1 <tatu.heinamaki@gmail.com>
2023-04-29 12:34:18 +0300

    Inventory and item added.

---
+++ Scripts/inventory.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+# Inventory fo items
+var inventory_items = []
+@export var pickup_volume: Area3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_3d_area_entered(area):
+	if area.is_in_group("item"):
+		inventory_items.append(area)
+		area.queue_free()
+		#print(inventory_items)
