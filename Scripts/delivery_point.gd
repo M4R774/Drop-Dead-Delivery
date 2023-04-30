5c9172a6073a941456fb5944ac0be68be092180c
Martta <martta_1994@hotmail.com>
2023-04-30 12:00:48 +0300

    Add visual feedback for packet delivery

--- Scripts/delivery_point.gd
+++ Scripts/delivery_point.gd
@@ -4,6 +4,9 @@ extends Area3D
 # Checks if player's inventory has a wanted item and gives points
 # Checking with name and having to write the name into two places correctly is dumb
 
+@export var keycap_1: Sprite3D
+@export var keycap_2: Sprite3D
+
 @export var delivery_id = ""
 @export var can_deliver_to = true
 var player_near = false
@@ -11,8 +14,19 @@ var player_near = false
 var player
 var inventory
 
-#func _ready():
-#	print(self.owner)
+
+func _ready():
+	keycap_1.visible = true
+	keycap_2.visible = false
+
+
+func _physics_process(_delta):
+	$Control/TextureProgressBar.value = ($Deliver.time_left / $Deliver.wait_time) * 100
+	if $Deliver.time_left <= 0:
+		$Control/TextureProgressBar.visible = false
+	else:
+		$Control/TextureProgressBar.visible = true
+
 
 func activate_delivery_point():
 	visible = true
@@ -22,28 +36,21 @@ func activate_delivery_point():
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		player_near = true
+		$Control.visible = player_near and can_deliver_to
 		player = body
 		inventory = body.get_node("Inventory")
-#		#if inventory.inventory_items.has(delivery_id):
-#		var items = inventory.inventory_items.size()
-#		if ites != 0:
-#			inventory.inventory_items = []
-#			#inventory.remove_from_inventory(delivery_id)
-#			body.add_score(100 * items)
-#			$Item_delivered.play()
-			# for now delivery points are in predefined places, no respawning them
-			#self.visible = false
-			#$Remover.start()
 
 
 func _on_body_exited(body):
 	if body.is_in_group("player"):
 		player_near = false
+		$Control.visible = player_near
 		$Deliver.stop()
 
 
 func _input(event):
 	if event.is_action_pressed("deliver_item") and player_near and can_deliver_to:
+		print("Started delivering")
 		$Deliver.start()
 	if event.is_action_released("deliver_item") and player_near:
 		$Deliver.stop()
@@ -68,4 +75,10 @@ func _on_deliver_timeout():
 		visible = false
 		can_deliver_to = false
 		get_tree().get_current_scene().remove_delivery_point(self.owner)
+	$Control.visible = player_near and can_deliver_to
 	
+
+
+func _on_keycap_blinker_timeout():
+	keycap_1.visible = !keycap_1.visible
+	keycap_2.visible = !keycap_2.visible
