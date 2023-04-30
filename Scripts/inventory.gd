da77c48dc24281c716c3044281c7606f8d1f432d
Martta <martta_1994@hotmail.com>
2023-04-30 18:16:27 +0300

    Add blip sound

--- Scripts/inventory.gd
+++ Scripts/inventory.gd
@@ -7,27 +7,17 @@ var small_items = []
 @export var pickup_volume: Area3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
-
 func _on_area_3d_area_entered(area):
 	if area.is_in_group("item") and inventory_items.size() < capacity:
 		inventory_items.append(area.item_name)
 		area.queue_free()
-		print(inventory_items)
 		if inventory_items.size() == 3: # all items are onboard
 			get_tree().get_current_scene().create_delivery_order() # calling gameplay to start the round
+		$blip.play()
 	if area.is_in_group("small_item"):
 		small_items.append(area.item_name)
 		area.queue_free()
-		print(inventory_items)
+		$blip.play()
 
 
 func remove_from_inventory(item_to_remove):
