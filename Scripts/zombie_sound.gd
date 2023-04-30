08bf77fa35b2d7540e07666a48dad2bbec4f5408
Martta <martta_1994@hotmail.com>
2023-04-30 17:48:56 +0300

    Bark fixes

--- Scripts/zombie_sound.gd
+++ Scripts/zombie_sound.gd
@@ -2,6 +2,7 @@ extends AudioStreamPlayer3D
 
 
 var sounds=[]
+var rng = RandomNumberGenerator.new()
 
 func _ready():
 	sounds.append(load("res://Sounds/Perttu/anna-tänne-se-helari.wav"))
@@ -9,6 +10,10 @@ func _ready():
 	sounds.append(load("res://Sounds/Perttu/paketti-paketti-helari.wav"))
 	sounds.append(load("res://Sounds/Perttu/siellä-se-on-helari.wav"))
 	sounds.append(load("res://Sounds/Perttu/siellä-se-onmurina.wav"))
+	$"../BarkDelay".start(rng.randf_range(1, 20))
+
+
+func _on_bark_delay_timeout():
 	play_random_sound()
 
 
@@ -16,3 +21,4 @@ func play_random_sound():
 	var sound = sounds.pick_random()
 	self.stream = sound
 	self.play()
+	$"../BarkDelay".start(rng.randf_range(10, 30))
