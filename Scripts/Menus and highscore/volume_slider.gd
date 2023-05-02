0e41998b959fb9150a1ba7dbfbe70f61ea40eb43
Martta <martta_1994@hotmail.com>
2023-05-02 20:06:42 +0300

    fix audio slider resetting on scene load

--- Scripts/Menus and highscore/volume_slider.gd
+++ Scripts/Menus and highscore/volume_slider.gd
@@ -5,7 +5,7 @@ var master_bus_index = AudioServer.get_bus_index("Master")
 
 func _ready():
 	connect("value_changed", _on_HSlider_value_changed)
-	AudioServer.set_bus_volume_db(master_bus_index, linear_to_db(self.value))
+	self.value = db_to_linear(AudioServer.get_bus_volume_db(master_bus_index))
 
 
 func _on_HSlider_value_changed(new_value):
